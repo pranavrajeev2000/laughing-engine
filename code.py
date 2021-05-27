3cd4dff496bc251d85c71e186985fe6992138a6f
Pranav Rajeev <pranavrajeev2000@yahoo.com>
2021-05-28 01:09:17 +0300

    helloe befoer gooodbye

--- code.py
+++ code.py
@@ -1,11 +1,2 @@
-def vp():
-    secret = "topsecret"
-    got = input("Enter password ")
-    
-    while got != secret:
-       got = input("Enter password ")
-       
-    print ("Success")
-    return True
-        
+
 
